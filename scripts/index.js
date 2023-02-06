132eb3eb6c205e41f8d41497369436611c0082f9
Ruslan Myachkov <37900140+rusmyachkov@users.noreply.github.com>
2023-02-07 01:41:10 +0500

    big update

--- scripts/index.js
+++ scripts/index.js
@@ -7,215 +7,177 @@ let homeMail = document.getElementById("homeMail")
 let homeTg = document.getElementById("homeTg")
 let homeLinkedin = document.getElementById("homeLinkedin")
 let homePinterest = document.getElementById("homePinterest")
-let sectionNav = document.getElementById("sectionNav")
 
 let contactsButton = document.getElementById("contactsButton")
 let worksButtons = document.getElementById("worksButtons")
 let notesButton = document.getElementById("notesButton")
 let photosButton = document.getElementById("photosButton")
 let aboutButton = document.getElementById("aboutButton")
+let closeButton = document.getElementById('closeButton')
 
 let sectionHome = document.getElementById("sectionHome")
 let sectionWorks = document.getElementById("sectionWorks")
 let sectionNotes = document.getElementById("sectionNotes")
 let sectionPhotos = document.getElementById("sectionPhotos")
 let sectionAbout = document.getElementById("sectionAbout")
+let sectionNav = document.getElementById("sectionNav")
+let sectionNavLinks = sectionNav.querySelector('.nav__links')
 let sectionContacts = document.getElementById("sectionContacts")
 
 let navRectangle = document.getElementById("navRectangle")
+let AboutTextGroup = document.querySelector('.about__text-group')
+
+let titleName = document.getElementById("title__ruslan")
+let titleSurname = document.getElementById("title__surname")
+let titleJob = document.getElementById("title__job")
+let titleDscr = document.getElementById("title__dscr")
+let titleLinkedin = document.getElementById("title__linkedin")
+
+
+const pages = [sectionHome, sectionWorks, sectionNotes, sectionPhotos, sectionAbout]
+closeButton.style.display = 'none'
+
+
+
+function showBlock(elementId) {
+for (let pageElement of pages) {
+  pageElement.style.opacity = '0'
+}
+
+let element = document.getElementById(elementId)
+element.style.opacity = '1'
+
+if (element === sectionHome) {
+  sectionHome.style.height = 'calc(100% - 244px)'
+  sectionHome.style.transition = 'height .3s ease'
+  sectionWorks.style.display = 'none'
+  sectionNotes.style.display = 'none'
+  sectionPhotos.style.display = 'none'
+  sectionAbout.style.display = 'none'
+  sectionNav.style.height = '208px'
+  sectionNav.style.gap = '20px'
+  sectionNavLinks.style['padding-right'] = '0'
+
+  worksButtons.style.display = 'block'
+  notesButton.style.display = 'block'
+  photosButton.style.display = 'block'
+  aboutButton.style.display = 'block'
+  closeButton.style.display = 'none'
+
+} else {
+  sectionHome.style.height = 'calc(100% - 106px)'
+  sectionNav.style.gap = '0'
+  sectionNav.style.height = '70px'
+  closeButton.style.display = 'block'
+  sectionNavLinks.style['width'] = 'fit-content'
+  sectionNavLinks.style['padding-right'] = '20px'
+
+  worksButtons.style.display = 'none'
+  notesButton.style.display = 'none'
+  photosButton.style.display = 'none'
+  aboutButton.style.display = 'none'
+  closeButton.style.display = 'block'
+}
+
+if (element === sectionWorks) {
+  sectionWorks.style.display = 'flex'
+  sectionNotes.style.display = 'none'
+  sectionPhotos.style.display = 'none'
+  sectionAbout.style.display = 'none'
+}
+
+if (element === sectionPhotos) {
+  sectionPhotos.style.display = 'flex'
+  sectionWorks.style.display = 'none'
+  sectionNotes.style.display = 'none'
+  sectionAbout.style.display = 'none'
+}
+
+if (element === sectionNotes) {
+  sectionNotes.style.display = 'flex'
+  sectionWorks.style.display = 'none'
+  sectionPhotos.style.display = 'none'
+  sectionAbout.style.display = 'none'
+}
+
+if (element === sectionAbout) {
+  sectionAbout.style.display = 'flex'
+  sectionNotes.style.display = 'none'
+  sectionWorks.style.display = 'none'
+  sectionPhotos.style.display = 'none'
+  AboutTextGroup.scrollTo(0,0)
+}
+
+  closeButton.textContent = 'Close ' + elementId.substr(7)
+}
 
 
 
 function showContacts() {
-  if(homeName.style.display === 'none') {
-    homeName.style.display = 'flex'
-    homeInst.style.display = 'none'
-    homeSurname.style.display = 'flex'
-    homeMail.style.display = 'none'
-    homeJob.style.display = 'flex'
-    homeTg.style.display = 'none'
-    homeDscr.style.display = 'flex'
+  if (titleName.textContent === 'instagram') {
+    titleName.textContent = 'ruslan'
+    titleSurname.textContent = 'myachkov'
+    titleJob.textContent = 'designer'
+
     homeLinkedin.style.display = 'none'
-    sectionNav.style.display = 'flex'
     homePinterest.style.display = 'none'
-    sectionHome.style.display = 'flex'
 
-    contactsButton.textContent = 'Get In Touch'
-  }
-
-  else {
-    homeName.style.display = 'none'
-    homeInst.style.display = 'flex'
-    homeSurname.style.display = 'none'
-    homeMail.style.display = 'flex'
-    homeJob.style.display = 'none'
-    homeTg.style.display = 'flex'
-    homeDscr.style.display = 'none'
-    homeLinkedin.style.display = 'flex'
-    sectionNav.style.display = 'none'
-    homePinterest.style.display = 'flex'
-    sectionHome.style.display = 'flex'
-
-    contactsButton.textContent = 'Close'
-
-
-    /*FOR WORKS*/
     sectionWorks.style.display = 'none'
-    notesButton.style.display = 'block'
-    photosButton.style.display = 'block'
-    aboutButton.style.display = 'block'
-    worksButtons.textContent = 'Works'
-
-     /*FOR ABOUT*/
-     sectionAbout.style.display = 'none'
-     worksButtons.style.display = 'block'
-     notesButton.style.display = 'block'
-     photosButton.style.display = 'block'
-     aboutButton.textContent = 'About'
-
-      /*FOR NOTES*/
-      navRectangle.style.background = '#BD2323'
-      notesButton.style.color = '#BD2323'
-      sectionNotes.style.display = 'none'
-      worksButtons.style.display = 'block'
-      photosButton.style.display = 'block'
-      aboutButton.style.display = 'block'
-      notesButton.textContent = 'Notes'
-
-       /*FOR PHOTOS*/
-       navRectangle.style.background = '#BD2323'
-       photosButton.style.color = '#BD2323'
-       sectionPhotos.style.display = 'none'
-       worksButtons.style.display = 'block'
-       notesButton.style.display = 'block'
-       aboutButton.style.display = 'block'
-       photosButton.textContent = 'Photos'
-
-  }
-}
-
-
-function showWorks() {
-  if(sectionHome.style.display === 'none') {
-    sectionHome.style.display = 'flex'
-    sectionWorks.style.display = 'none'
-    notesButton.style.display = 'block'
-    photosButton.style.display = 'block'
-    aboutButton.style.display = 'block'
-
-    worksButtons.textContent = 'Works'
-  }
-
-  else {
-    sectionHome.style.display = 'none'
-    sectionWorks.style.display = 'flex'
-    notesButton.style.display = 'none'
-    photosButton.style.display = 'none'
-    aboutButton.style.display = 'none'
-
-    worksButtons.textContent = 'Close Works'
-  }
-}
-
-/*
-function showWorks() {
-  if(sectionHome.style.display === 'none') {
-    sectionHome.style.display = 'flex'
-    sectionWorks.style.display = 'none'
-    notesButton.style.display = 'block'
-    photosButton.style.display = 'block'
-    aboutButton.style.display = 'block'
-
-    worksButtons.textContent = 'Works'
-  }
-
-  else {
-    sectionHome.style.display = 'none'
-    sectionWorks.style.display = 'flex'
-    notesButton.style.display = 'none'
-    photosButton.style.display = 'none'
-    aboutButton.style.display = 'none'
-
-    worksButtons.textContent = 'Close Works'
-  }
-}
-*/
-
-
-function showNotes() {
-  if(sectionHome.style.display === 'none') {
-    sectionHome.style.display = 'flex'
     sectionNotes.style.display = 'none'
-    worksButtons.style.display = 'block'
-    photosButton.style.display = 'block'
-    aboutButton.style.display = 'block'
-
-    navRectangle.style.background = '#BD2323'
-    notesButton.style.color = '#BD2323'
-    notesButton.textContent = 'Notes'
-  }
-
-  else {
-    sectionHome.style.display = 'none'
-    sectionNotes.style.display = 'flex'
-    worksButtons.style.display = 'none'
-    photosButton.style.display = 'none'
-    aboutButton.style.display = 'none'
-
-    navRectangle.style.background = '#2E3192'
-    notesButton.style.color = '#2E3192'
-    notesButton.textContent = 'Close Notes'
-  }
-}
-
-
-function showPhotos() {
-  if(sectionHome.style.display === 'none') {
-    sectionHome.style.display = 'flex'
     sectionPhotos.style.display = 'none'
-    worksButtons.style.display = 'block'
-    notesButton.style.display = 'block'
-    aboutButton.style.display = 'block'
-
-    navRectangle.style.background = '#BD2323'
-    photosButton.style.color = '#BD2323'
-    photosButton.textContent = 'Photos'
-  }
-
-  else {
-    sectionHome.style.display = 'none'
-    sectionPhotos.style.display = 'flex'
-    worksButtons.style.display = 'none'
-    notesButton.style.display = 'none'
-    aboutButton.style.display = 'none'
-
-    navRectangle.style.background = '#FEF200'
-    photosButton.style.color = '#FEF200'
-    photosButton.textContent = 'Close Photos'
-  }
-}
-
-
-function showAbout() {
-  if(sectionHome.style.display === 'none') {
-    sectionHome.style.display = 'flex'
     sectionAbout.style.display = 'none'
 
+    sectionNav.style.display = 'flex'
+    sectionNav.style.height = '208px'
+    sectionNav.style.gap = '20px'
+    sectionNavLinks.style['padding-right'] = '0'
+
     worksButtons.style.display = 'block'
     notesButton.style.display = 'block'
     photosButton.style.display = 'block'
+    aboutButton.style.display = 'block'
 
-    aboutButton.textContent = 'About'
+    navRectangle.style['background'] = '#BD2323'
+
+    homeDscr.style.display = 'flex'
+    homeLinkedin.style.height = '100%'
+
+    sectionHome.style.height = 'calc(100% - 244px)'
+
+    contactsButton.style['paddingLeft'] = '8px'
+    contactsButton.textContent = 'Get in Touch'
+
+    sectionContacts.style['gap'] = '0'
+
+    closeButton.style.display = 'none'
+
+
+  } else {
+    titleName.textContent = 'instagram'
+    titleName.innerHTML = '<a class="home__title home__title_orange" id="title__ruslan" href="https://visuelle.co.uk/episode-7">instagram</a>'
+    titleSurname.textContent = 'mail'
+    titleJob.textContent = 'telegram'
+
+    homeDscr.style.display = 'none'
+    homeLinkedin.style.display = 'flex'
+    homePinterest.style.display = 'flex'
+
+    sectionWorks.style.display = 'none'
+    sectionNotes.style.display = 'none'
+    sectionPhotos.style.display = 'none'
+    sectionAbout.style.display = 'none'
+    sectionNav.style.display = 'none'
+
+    sectionContacts.style['gap'] = '8px'
+    contactsButton.style['paddingLeft'] = '0'
+    contactsButton.textContent = 'Close'
+
+    sectionHome.style.display = 'flex'
+    sectionHome.style.opacity = '1'
+    sectionHome.style.height = 'calc(100% - 36px)'
+    sectionHome.style.transition = 'none'
+  }
   }
 
-  else {
-    sectionHome.style.display = 'none'
-    sectionAbout.style.display = 'flex'
 
-    worksButtons.style.display = 'none'
-    notesButton.style.display = 'none'
-    photosButton.style.display = 'none'
 
-    aboutButton.textContent = 'Close About'
-  }
-}
