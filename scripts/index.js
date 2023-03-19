b67dea2a53bcffbca77dd01b1caca25d911dc199
Ruslan Myachkov <37900140+rusmyachkov@users.noreply.github.com>
2023-03-19 21:55:36 +0500

    Lazy load Works

--- scripts/index.js
+++ scripts/index.js
@@ -51,12 +51,19 @@ let projectTeko = document.getElementById("project__teko")
 let projectEasymusic = document.getElementById("project__easymusic")
 let projectAfisha = document.getElementById("project__afisha")
 
-console.log(projectAfisha)
+
+const imageObserver = new IntersectionObserver(
+  (entries, observer) => {
+    entries.forEach((entry) => {
+      entry.target.src = entry.target.dataset.src;
+      observer.unobserve(entry.target);
+    });
+  },
+)
+
 
 const pages = [sectionHome, sectionWorks, sectionNotes, sectionPhotos, sectionAbout]
 closeButton.style.display = 'none'
-
-
 
 function showBlock(elementId) {
 for (let pageElement of pages) {
@@ -77,7 +84,6 @@ if (element === sectionHome) {
   sectionNav.style.height = '208px'
   sectionNav.style.gap = '20px'
   sectionNavLinks.style['padding-right'] = '0'
-
   worksButtons.style.display = 'block'
   notesButton.style.display = 'block'
   photosButton.style.display = 'block'
@@ -109,6 +115,7 @@ if (element === sectionWorks) {
   contactsButton.style.display = 'none'
   projectsButton.style.display = 'block'
   sectionProjects.style.display = 'none'
+  element.querySelectorAll("img").forEach((image) => imageObserver.observe(image));
 } else {
   contactsButton.style.display = 'block'
   projectsButton.style.display = 'none'
@@ -133,6 +140,7 @@ if (element === sectionNotes) {
   sectionPhotos.style.display = 'none'
   sectionAbout.style.display = 'none'
   notesGrid.scrollTo(0,0)
+  element.querySelectorAll("img").forEach((image) => imageObserver.observe(image));
 }
 
 if (element === sectionAbout) {
@@ -410,39 +418,6 @@ document.addEventListener("DOMContentLoaded", function() {
     });
   }
 });
-
-
-(function() {
-  // Initialize Intersection Observer. The argument passed here is the callback
-  // function that should be run when the observer is triggered.
-  var observer = new IntersectionObserver(onIntersect);
-  // Observe every element with the "data-lazy-load" attribute for it to
-  // intersect the screen.
-  document.querySelectorAll('[data-lazy-load]').forEach(function(img) {
-    observer.observe(img);
-  });
-
-  // This is the callback function when the observer is triggered. entries is an
-  // array of all observable elements for which the function was triggered, and
-  // observer is our observer instance.
-  function onIntersect(entries, observer) {
-    // Step through each entry in the entries array ...
-    entries.forEach(function(entry) {
-      // Don't do anything if the element has already been processed or if it
-      // isn't currently intersecting. The Intersection Observer also fires when
-      // an element leaves the viewport, which is why we need this check.
-      if (entry.target.getAttribute('data-processed') || !entry.isIntersecting) return true;
-      // Set the images source to the value of the "data-source" attribute. This
-      // is why we were storing the source we ultimately want to load in a data
-      // attribute.
-      entry.target.setAttribute('src', entry.target.getAttribute('data-src'));
-      // Add a new attribute to the image called "data-processed" and set it to
-      // true. We do this so we only process each element a single time and we
-      // don't try to reload an image that's already been loaded.
-      entry.target.setAttribute('data-processed', true);
-    });
-  }
-})();
 
 
 
